0b43e4829a8591020832615c4f72cf91ccaa0403
Phoshizzle23 <mattywoodward88@gmail.com>
2023-11-07 09:06:47 +1100

    Add project files.

---
+++ EmployeeMGTBlazor/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeMGTBlazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeMGTBlazor.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeMGTBlazor/Migrations/20231106004844_3rd.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeMGTBlazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class _3rd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "EmailAddress",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "Employees");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
